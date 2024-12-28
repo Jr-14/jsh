49aeadc97b33bbc089de0c3e448a96d2ce388776
John Emerson Requizo <jr.jemerson14@gmail.com>
2024-12-29 08:50:10 +1100

    Add initial paths for where binaries are to be executed

--- src/main.c
+++ src/main.c
@@ -10,18 +10,14 @@ void parseInput(char input[], DynamicArray *strArray) {
     int ptr = 0;
     int strSize = 0;
     char str[BUFF_SIZE];
-    // unsigned long index = ptr * sizeof(char);
     while (input[ptr] != '\0' && ptr < BUFF_SIZE - 1) {
         str[strSize] = input[ptr];
-        printf("Char: %c, size: %d\n", str[strSize], strSize);
-
         ptr++;
         strSize++;
 
         if (str[strSize - 1] == ' ') {
             str[strSize - 1] = '\0';
             char *newStr = strdup(str);
-            printf("The new string is: %s\n", newStr);
             insert(strArray, (void *)newStr);
             strSize = 0;
             str[0] = '\0';
@@ -31,34 +27,48 @@ void parseInput(char input[], DynamicArray *strArray) {
     if (str[strSize - 1] == '\n') {
         str[strSize - 1] = '\0';
         char *newStr = strdup(str);
-        // printf("The new string is: %s\n", newStr);
         insert(strArray, (void *)newStr);
     } 
 }
 
+void debugDynArr(DynamicArray *inputs, char *name) {
+    printf("---------- %s ----------\n", name);
+    if (inputs->size) {
+        for (unsigned int i = 0; i < inputs->size; i++) {
+            char *rs = (char *)getFromArray(inputs, i);
+            printf("ind: %d -- %s\n", i, rs);
+        }
+    }
+}
+
+void initialisePath(DynamicArray *path) {
+    insert(path, "/bin");
+    insert(path, "/usr/bin");
+    debugDynArr(path, "Initialised Paths");
+}
+
 int main(int argc, char *argv[]) {
-    DynamicArray strArray;
-    initArray(&strArray, 10, sizeof(char*));
+    DynamicArray inputs, path;
+    initArray(&inputs, 10, sizeof(char*));
+    initArray(&path, 10, sizeof(char*));
+
+    initialisePath(&path);
+
     char input[BUFF_SIZE];
     while (true) {
         printf("jsh> ");
-        if (fgets(input, BUFF_SIZE, stdin) != NULL) {
-            // printf("Hello string: %s", input);
-            parseInput(input, &strArray);
-        } else {
-            printf("Error");
+        if (fgets(input, BUFF_SIZE, stdin) == NULL) {
+            fprintf(stderr, "Error reading command");
             exit(1);
         }
 
-        if (strArray.size) {
-            for (unsigned int i = 0; i < strArray.size; i++) {
-                char *rs = (char *)getFromArray(&strArray, i);
-                printf("Last item from array is: %s\n", rs);
-            }
-        }
+        parseInput(input, &inputs);
+        debugDynArr(&inputs, "Received Commands");
+        freeArray(&inputs);
+        initArray(&inputs, 10, sizeof(char*));
     }
 
-    freeArray(&strArray);
+    freeArray(&inputs);
     return 0;
 }
 
