ee1e14b8ed59c6b06f81e8371b22ba098b666ec1
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-01-02 22:12:39 +1100

    Wow a full working shell for single commands with flags!

--- src/main.c
+++ src/main.c
@@ -14,6 +14,83 @@ typedef struct {
     DynamicArray *path;
 } ShellConfig;
 
+typedef struct {
+    char *command;
+    char *path;
+    int argc;
+    bool builtIn;
+    char **argv;
+} Executable;
+
+int createExecutable(DynamicArray *inputs, ShellConfig *config, Executable *e) {
+    e->command = strdup(getFromArray(inputs, 0));
+    e->path = NULL;
+    e->argc = 0;
+    e->builtIn = false;
+
+    e->argv = malloc((inputs->size + 1) * sizeof(char*));
+    if (e->argv == NULL) {
+        perror("malloc failed for argv");
+        exit(1);
+    }
+
+    for (unsigned int i = 0; i <inputs->size; i++) {
+        e->argv[i] = strdup(getFromArray(inputs, i));
+        if (e->argv[i] == NULL) {
+            perror("strdup failed");
+            exit(1);
+        }
+        e->argc++;
+    }
+    e->argv[inputs->size] = NULL;
+
+    if (strcmp(e->command, "exit") == 0
+        || strcmp(e->command, "cd") == 0
+        || strcmp(e->command, "path") == 0)
+    {
+        e->builtIn = true;
+        return 0;
+    }
+
+    for (unsigned int i = 0; i < config->path->size; i++) {
+        char buf[256];
+        char *path = strdup(getFromArray(config->path, i));
+        snprintf(buf, sizeof(buf), "%s/%s", path, e->command);
+        printf("Full path is %s\n", buf);
+
+        if (access(buf, X_OK) == 0) {
+            e->path = strdup(buf);
+            if (e->path == NULL) {
+                perror("strdup failed for path");
+                exit(1);
+            }
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+void freeExecutable(Executable *e) {
+    if (e->argv) {
+        for (int i = 0; i < e->argc; i++) {
+            free(e->argv[i]);
+        }
+        free(e->argv);
+        e->argv = NULL;
+    }
+    if (e->path) {
+        free(e->path);
+        e->path = NULL;
+    }
+    if (e->command) {
+        free(e->command);
+        e->command = NULL;
+    }
+    e->argc = 0;
+    e->builtIn = false;
+}
+
 void parseInput(char input[], DynamicArray *strArray) {
     int ptr = 0;
     int strSize = 0;
@@ -49,12 +126,6 @@ void debugDynArr(DynamicArray *inputs, char *name) {
     }
 }
 
-void initialisePath(DynamicArray *path) {
-    insert(path, "/bin");
-    insert(path, "/usr/bin");
-    debugDynArr(path, "Initialised Paths");
-}
-
 void initialiseShellConfig(ShellConfig *config) {
     config->path = malloc(sizeof(DynamicArray));
     if (config->path == NULL) {
@@ -64,6 +135,7 @@ void initialiseShellConfig(ShellConfig *config) {
     initArray(config->path, 10, sizeof(char*));
     insert(config->path, "/bin");
     insert(config->path, "/usr/bin");
+    debugDynArr(config->path, "Initial paths");
 
     config->cwd = getenv("HOME");
     if (config->cwd == NULL) {
@@ -73,22 +145,20 @@ void initialiseShellConfig(ShellConfig *config) {
     chdir(config->cwd);
 };
 
-int executeBuiltInCommands(DynamicArray *commands, ShellConfig *config) {
+int executeBuiltIn(Executable *e, ShellConfig *config) {
     int ERR = -1;
     int NO_RUN = 0;
     int OK = 1;
 
-    char *command = getFromArray(commands, 0);
-
-    if (strcmp(command, "exit") == 0) {
+    if (strcmp(e->command, "exit") == 0) {
         exit(0);
         return ERR;
-    } else if (strcmp(command, "cd") == 0) {
-        char *path = getFromArray(commands, 1);
+    } else if (strcmp(e->command, "cd") == 0) {
+        char *path = e->argv[1];
         chdir(path);
         config->cwd = getcwd(config->cwd, 256 * sizeof(char));
         return OK;
-    } else if (strcmp(command, "path") == 0) {
+    } else if (strcmp(e->command, "path") == 0) {
         char *path = getenv("PATH");
         printf("%s\n", path);
         return OK;
@@ -96,85 +166,57 @@ int executeBuiltInCommands(DynamicArray *commands, ShellConfig *config) {
     return NO_RUN;
 }
 
-int readCommand(char *command, ShellConfig *config, char *commandPath[]) {
-    for (unsigned int i = 0; i < config->path->size; i++) {
-        char buf[256];
-        char *path = strdup(getFromArray(config->path, i));
-        snprintf(buf, sizeof(buf), "%s/%s", path, command);
-        printf("Full path is %s\n", buf);
-
-        int errno = access(buf, X_OK);
-        if (errno == 0) {
-            commandPath[0] = malloc(256 * sizeof(char));
-            strncpy(commandPath[0], strdup(buf), 256);
-            return 0;
-        }
-    }
-
-    return -1;
-}
-
 int main(int argc, char *argv[]) {
     ShellConfig config;
-    DynamicArray inputs;
-    initArray(&inputs, 10, sizeof(char*));
-
     initialiseShellConfig(&config);
 
-    char input[BUFF_SIZE];
     while (true) {
+        char input[BUFF_SIZE];
         printf("jsh %s> ", config.cwd);
+
         if (fgets(input, BUFF_SIZE, stdin) == NULL) {
             fprintf(stderr, "Error reading command");
             exit(1);
         }
 
+        DynamicArray inputs;
+        Executable exc;
+
+        initArray(&inputs, 10, sizeof(char*));
         parseInput(input, &inputs);
         debugDynArr(&inputs, "Received Commands");
 
-        // TODO-JR: Read and parse arguments for command
-        char *strCommand = getFromArray(&inputs, 0);
+        createExecutable(&inputs, &config, &exc);
 
-        char *command[BUFF_SIZE];
-        command[0] = strCommand;
-        command[1] = NULL;
-
-        int builtInCommandStatus = executeBuiltInCommands(&inputs, &config);
-        printf("Built in command status: %d\n", builtInCommandStatus);
-        if (builtInCommandStatus == -1) {
-            fprintf(stderr, "Failed to execute built in command '%s'", command[0]);
-            return -1;
-        } else if (builtInCommandStatus == 0) {
-            int errnoReadCommand = readCommand(strCommand, &config, command);
-            if (errnoReadCommand < 0) {
-                fprintf(stderr, "Error accessing command: %s\n", strCommand);
+        if (exc.builtIn) {
+            int builtInCommandStatus = executeBuiltIn(&exc, &config);
+            printf("Built in command status: %d\n", builtInCommandStatus);
+            if (builtInCommandStatus == -1) {
+                fprintf(stderr, "Failed to execute built in command '%s'", (char *)getFromArray(&inputs, 0));
+                return -1;
             }
-
-            printf("Full command with arguments is %s\n", *command);
-
+        } else {
             int status;
-
             int pid = fork();
             if (pid < 0) {
                 fprintf(stderr, "Failed to fork process\n");
             } else if (pid == 0) {
                 // Child process
-                int execStatus = execv(command[0], command);
+                int execStatus = execv(exc.path, exc.argv);
                 if (execStatus == -1) {
                     fprintf(stderr, "Failed to exec process\n");
                     return 1;
                 }
                 return 0;
             } else {
-                wait(&status);
+                wait(&pid);
             }
         }
 
         freeArray(&inputs);
-        initArray(&inputs, 10, sizeof(char*));
+        freeExecutable(&exc);
     }
 
-    freeArray(&inputs);
     return 0;
 }
 
